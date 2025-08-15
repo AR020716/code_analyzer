803fdcf746ba72fbabd2b0cf7f0f6ee1b19032b9
Nilankur <bbjilipi1@gmail.com>
2025-08-15 23:54:02 +0530

    Initial commit

---
+++ code analyzer test/F.java
@@ -0,0 +1,28 @@
+package F;
+
+import java.util.*;
+
+public class F {
+	
+	public static void main(String[] args) {
+		
+		HashSet<Integer> mySet = new HashSet<>();
+		mySet.add(9);
+		mySet.add(10);
+		mySet.add(11);
+		
+		String f = "";
+		
+		int n = 19;
+		
+		for (int i=1; i <=n; i++) {
+			f = f + "*";
+			if (i>15 || mySet.contains(i)) {
+				f = f + "\n";
+			}
+		}
+		
+		System.out.println(f);
+	}
+
+}
