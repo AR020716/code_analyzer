31ac4e6b1ed618094c82a7866fd291c755ea36e9
Anushri Roy <anushri7703@gmail.com>
2025-08-16 11:48:37 +0530

    Finds java files

--- CodeAnalyzer.java
+++ CodeAnalyzer.java
@@ -1,7 +1,29 @@
-import java.util.*;
- class CodeAnalyzer
- {
-    public static void main(){
-        System.out.println("hello from anu")
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeAnalyzer {
+    public static void main(String[] args) {
+        File folder = new File("code_analyzer/code analyzer test");
+        List<File> tempList = new ArrayList<>();
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.getName().endsWith(".java")) {
+                   tempList.add(file); 
+                }
+            }
+        }
+        else {
+            System.out.println("No file in folder");
+        }
+        File[] temp = tempList.toArray(new File[0]);
+        if (temp != null) {
+            for (File file : temp) {
+                System.out.println("Found Java file: " + file.getName());
+            }
+        } else {
+            System.out.println("No .java files found.");
+        }
     }
- }
+}
