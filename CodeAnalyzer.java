82c19527422a796ca68b3cceed5dab0b118136de
Nilankur <bbjilipi1@gmail.com>
2025-08-16 13:04:44 +0530

    refactored file finder

--- CodeAnalyzer.java
+++ CodeAnalyzer.java
@@ -1,7 +1,29 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CodeAnalyzer{
-    public static void main (String args[]){
-        System.out.println("Hello world");
+public class CodeAnalyzer {
+    public static void main(String[] args) {
+        File folder = new File("code analyzer test");
+        List<File> tempList = new ArrayList<>();
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.getName().endsWith(".java")) {
+                   tempList.add(file); 
+                }
+            }
+            File[] temp = tempList.toArray(new File[0]);
+            if (!tempList.isEmpty()) {
+                for (File file : temp) {
+                    System.out.println("Found Java file: " + file.getName());
+                }
+            } else {
+                System.out.println("No .java files found.");
+            }
+        }
+        else {
+            System.out.println("No folder found.");
+        }
     }
 }
