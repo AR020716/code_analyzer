d9b1c5ba1377bef8f16575d1c7288600a0672309
Nilankur <bbjilipi1@gmail.com>
2025-08-18 20:15:39 +0530

    Reading all lines from files

--- CodeAnalyzer.java
+++ CodeAnalyzer.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
+import java.nio.file.Files;
 
 public class CodeAnalyzer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File folder = new File("code analyzer test");
         List<File> tempList = new ArrayList<>();
         File[] files = folder.listFiles();
@@ -17,6 +19,13 @@ public class CodeAnalyzer {
             if (!tempList.isEmpty()) {
                 for (File file : temp) {
                     System.out.println("Found Java file: " + file.getName());
+
+                    List<String> code = Files.readAllLines(file.toPath());
+                    for (String line : code){
+                        System.out.println(line);
+                    } 
+
+                    System.out.println();
                 }
             } else {
                 System.out.println("No .java files found.");
