f26e788e1697af8c27900c72ca07f6d662cc0f89
Anushri Roy <anushri7703@gmail.com>
2025-08-18 21:56:12 +0530

    Class checker

--- CodeAnalyzer.java
+++ CodeAnalyzer.java
@@ -1,10 +1,33 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
+import java.util.regex.*;
 public class CodeAnalyzer {
+    public static void ClassnameChecker(List<String> code) { 
+        Pattern classPattern = Pattern.compile("\\bclass\\s+([A-Za-z0-9_$]+)");
+        
+        Pattern validClassPattern = Pattern.compile("[A-Z][a-zA-Z0-9]*");
+        boolean isClass = false;
+                for (String line : code) {
+                Matcher matcher = classPattern.matcher(line);
+                if (matcher.find()) {
+                isClass = true;
+                String className = matcher.group(1);
+                if (validClassPattern.matcher(className).matches()) {
+                    System.out.println(className + " is a valid class name");
+                    
+                } else {
+                    System.out.println(className + " is an invalid class name");
+                }
+              
+            }
+         }
+         if(!isClass){
+            System.out.println("No class declaration found in this file ");
+         }
+    }
+
     public static void main(String[] args) throws IOException {
         File folder = new File("code analyzer test");
         List<File> tempList = new ArrayList<>();
@@ -20,9 +43,7 @@ public class CodeAnalyzer {
             for (File file : temp) {
                 System.out.println("Found Java file: " + file.getName());
             List<String> code = Files.readAllLines(file.toPath());
-            for(String line : code){
-                System.out.println(line);
-            }
+            ClassnameChecker(code);
             System.out.println();
             }
         } else {
@@ -33,4 +54,6 @@ public class CodeAnalyzer {
             System.out.println("No folder found");
         }
     }
+    
 }
+
---
+++ code analyzer test/NoClass.java
@@ -0,0 +1,5 @@
+package code analyzer test;
+
+public NoClass {
+    
+}
