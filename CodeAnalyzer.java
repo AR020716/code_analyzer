2549f7fe788e0aab522e30f420d586c7e8458a3f
Nilankur <bbjilipi1@gmail.com>
2025-08-18 21:54:47 +0530

    class name checker added

--- CodeAnalyzer.java
+++ CodeAnalyzer.java
@@ -1,10 +1,34 @@
 import java.io.File;
-import java.util.ArrayList;
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
+import java.util.regex.*;
 import java.nio.file.Files;
 
 public class CodeAnalyzer {
+
+    public static void ClassNameChecker(List<String> code){
+        Pattern classPattern = Pattern.compile("\\bclass\\s+([A-Za-z0-9_$]+)");
+        Pattern validClassPattern = Pattern.compile("[A-Z][a-zA-Z0-9]*");
+
+        boolean classFlag = false;
+
+        for (String line : code){
+            Matcher classMatcher = classPattern.matcher(line);
+            if (classMatcher.find()){
+                classFlag = true;
+                String className = classMatcher.group(1);
+                if (validClassPattern.matcher(className).matches()){
+                    System.out.println("Valid class name: " +className);
+                }
+                else{
+                    System.out.println("Invalid class name: " +className);
+                }
+            }
+        }
+        if (!classFlag){
+            System.out.println("No class declaration in file.");
+        }
+    }
     public static void main(String[] args) throws IOException {
         File folder = new File("code analyzer test");
         List<File> tempList = new ArrayList<>();
@@ -19,12 +43,8 @@ public class CodeAnalyzer {
             if (!tempList.isEmpty()) {
                 for (File file : temp) {
                     System.out.println("Found Java file: " + file.getName());
-
                     List<String> code = Files.readAllLines(file.toPath());
-                    for (String line : code){
-                        System.out.println(line);
-                    } 
-
+                    ClassNameChecker(code);
                     System.out.println();
                 }
             } else {
---
+++ code analyzer test/InvalidClass.java
@@ -0,0 +1,3 @@
+public class invalidClass {
+    System.out.println ("Invalid classname");
+}
