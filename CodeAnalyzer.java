2a40712e50b802201b2ccfd7e8298cc33b4e6928
Anushri Roy <anushri7703@gmail.com>
2025-08-19 22:28:15 +0530

    semicolon checker added

--- CodeAnalyzer.java
+++ CodeAnalyzer.java
@@ -50,6 +50,29 @@ public class CodeAnalyzer {
                 } 
             }
         }
+        public static void SemicolonChecker(List<String> code){
+        
+        String skipPattern = "[{}]|(if|else|for|while|switch|try|catch).|[a-zA-Z0-9_\\s]:|.*\\{";             
+        
+        boolean missedSemicolonFlag = false;
+
+        for (int i = 0; i < code.size(); i++) {
+            String line = code.get(i).trim();
+
+            if (line.isEmpty() || line.matches(skipPattern)) {
+                continue;
+            }
+
+            if (!line.endsWith(";")) {
+                missedSemicolonFlag = true;
+                System.out.println("Missing semicolon at line: "+(i+1));
+            }
+        }
+
+        if (!missedSemicolonFlag) {
+            System.out.println("No missing semicolon detected.");
+        }
+    }
          public static void main(String[] args) throws IOException {
         File folder = new File("code analyzer test");
         List<File> tempList = new ArrayList<>();
@@ -72,6 +95,7 @@ public class CodeAnalyzer {
             }
             ClassnameChecker(filterCode);
             VariableNameChecker(filterCode);
+            SemicolonChecker(filterCode);
             System.out.println();
             }
         } else {
--- code analyzer test/test.java
+++ code analyzer test/test.java
@@ -1,5 +1,5 @@
 public class test {
      int A; 
-     System.out.println(A);
-     System.out.println("invalid class checking");
+     System.out.println(A)
+     System.out.println("invalid class checking")
 }
